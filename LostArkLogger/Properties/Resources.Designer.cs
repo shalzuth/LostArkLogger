bdaa6b02ebb7a9e0c4f55fb625694121c9b36f4d
shalzuth <shalzuth@users.noreply.github.com>
2022-05-05 16:12:14 -0700

    fix packet sniffing
    still need to add configurability

--- LostArkLogger/Properties/Resources.Designer.cs
+++ LostArkLogger/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace LostArkLogger.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap class_symbol_0 {
+            get {
+                object obj = ResourceManager.GetObject("class_symbol_0", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] GameMsg_English {
