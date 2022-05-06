dcc7b52b0643465c0cf377b1a5e3a79d497378e9
Aleksi Kytölä <kytola.aleksi@gmail.com>
2022-05-06 15:36:42 +0300

    Use universal time format

--- LostArkLogger/Data/LogInfo.cs
+++ LostArkLogger/Data/LogInfo.cs
@@ -14,17 +14,8 @@ namespace LostArkLogger
         public Boolean FrontAttack { get; set; }
         public override string ToString()
         {
-            return Time.ToString("yy:MM:dd:HH:mm:ss.f") + "," +
+            return Time.ToUniversalTime() + "," +
                    Source + "," +
-                   Destination + "," +
-                   SkillName + "," +
-                   Damage + "," +
-                   (Crit ? "1" : "0") + "," +
-                   (BackAttack ? "1" : "0") + "," +
-                   (FrontAttack ? "1" : "0");
-            return Time.ToString("yy:MM:dd:HH:mm:ss.f") + "," +
-                   Source + "," +
-                   PC + "," +
                    Destination + "," +
                    SkillName + "," +
                    Damage + "," +
