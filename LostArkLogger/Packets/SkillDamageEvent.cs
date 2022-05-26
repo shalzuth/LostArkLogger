50bdeb2722e3e5e5fda3ebeeb24a98aefe594d5c
shalzuth <shalzuth@users.noreply.github.com>
2022-05-25 18:33:28 -0700

    skill buff fix
    skill buff fix

--- LostArkLogger/Packets/SkillDamageEvent.cs
+++ LostArkLogger/Packets/SkillDamageEvent.cs
@@ -7,24 +7,24 @@ namespace LostArkLogger
         public SkillDamageEvent(BitReader reader)
         {
             Damage = reader.ReadPackedInt();
-            CurrentHealth = reader.ReadPackedInt();
+            MaxHealth = reader.ReadPackedInt();
             Modifier = reader.ReadByte();
             TargetId = reader.ReadUInt64();
             hasfield4 = reader.ReadByte();
             if (hasfield4 == 1)
                 field4 = reader.ReadByte();
             field5 = reader.ReadByte();
-            MaxHealth = reader.ReadPackedInt();
+            CurrentHealth = reader.ReadPackedInt();
             field7 = reader.ReadUInt16();
         }
         public Int64 Damage;
-        public Int64 CurrentHealth;
+        public Int64 MaxHealth;
         public Byte Modifier;
         public UInt64 TargetId;
         public Byte hasfield4;
         public Byte field4;
         public Byte field5;
-        public Int64 MaxHealth;
+        public Int64 CurrentHealth;
         public UInt16 field7;
     }
 }
