1302d9316b02d06439e3a3bb17bb4f8a2cab6e06
shalzuth <shalzuth@users.noreply.github.com>
2022-07-22 15:25:57 -0700

    npc summon fix
    npc summon fix

--- LostArkLogger/Packets/Steam/PKTNewNpcSummon.cs
+++ LostArkLogger/Packets/Steam/PKTNewNpcSummon.cs
@@ -7,9 +7,9 @@ namespace LostArkLogger
         public void SteamDecode(BitReader reader)
         {
             npcStruct = reader.Read<NpcStruct>();
-            bytearray_1 = reader.ReadBytes(6);
+            bytearray_1 = reader.ReadBytes(9);
             OwnerId = reader.ReadUInt64();
-            bytearray_0 = reader.ReadBytes(25);
+            bytearray_0 = reader.ReadBytes(22);
             b_0 = reader.ReadByte();
         }
     }
