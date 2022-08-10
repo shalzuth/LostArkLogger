6156f8d89cd66776db586a97f3f2da293eb044a3
shalzuth <shalzuth@users.noreply.github.com>
2022-08-10 12:25:29 -0700

    korea extra fields

--- LostArkLogger/Packets/Base/NpcStruct.cs
+++ LostArkLogger/Packets/Base/NpcStruct.cs
@@ -15,6 +15,7 @@ namespace LostArkLogger
         public List<StatusEffectData> statusEffectDatas;
         public List<subPKTInitPC29> subPKTInitPC29s;
         public List<UInt64> u64list_0;
+        public List<Byte> blist_0;
         public List<Byte[]> bytearraylist_0;
         public StatPair statPair;
         public UInt64 u64_0;
