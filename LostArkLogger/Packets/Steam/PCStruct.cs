200aedd4fbb1110834ef1fc4d59cbd89d91719ac
guy0090 <4467272+guy0090@users.noreply.github.com>
2022-09-29 19:10:47 +0200

    [Steam] Fixes PCStruct.cs
    Adds definitions from comment by @rivellathetank on https://github.com/shalzuth/LostArkLogger/commit/93211b814cb7211e6a4aa53f5ddc3f38460210c3

--- LostArkLogger/Packets/Steam/PCStruct.cs
+++ LostArkLogger/Packets/Steam/PCStruct.cs
@@ -8,14 +8,14 @@ namespace LostArkLogger
         {
             b_0 = reader.ReadByte();
             u32_0 = reader.ReadUInt32();
-            u16_1 = reader.ReadUInt16();
+            ClassId = reader.ReadUInt16();
             EquippedItems = reader.ReadList<ItemInfo>();
             b_12 = reader.ReadByte();
-            Name = reader.ReadString();
-            u16_5 = reader.ReadUInt16();
             str_0 = reader.ReadString();
+            u16_3 = reader.ReadUInt16();
+            Name = reader.ReadString();
+            u32_7 = reader.ReadUInt32();
             u32_8 = reader.ReadUInt32();
-            u32_9 = reader.ReadUInt32();
             b_1 = reader.ReadByte();
             b_2 = reader.ReadByte();
             b_3 = reader.ReadByte();
@@ -33,25 +33,25 @@ namespace LostArkLogger
             b_8 = reader.ReadByte();
             u32_3 = reader.ReadUInt32();
             b_9 = reader.ReadByte();
-            u64_0 = reader.ReadUInt64();
+            PartyId = reader.ReadUInt64();
             b_10 = reader.ReadByte();
             bytearray_0 = reader.ReadBytes(25);
             itemInfos = reader.ReadList<ItemInfo>();
-            u64_1 = reader.ReadUInt64();
+            u64_0 = reader.ReadUInt64();
             u32_4 = reader.ReadUInt32();
-            u16_2 = reader.ReadUInt16();
+            Level = reader.ReadUInt16();
             skillRunes = reader.Read<SkillRunes>();
             u32_5 = reader.ReadUInt32();
             subPKTNewNpc5 = reader.Read<subPKTNewNpc5>();
-            u16_3 = reader.ReadUInt16();
+            u16_1 = reader.ReadUInt16();
             statusEffectDatas = reader.ReadList<StatusEffectData>();
             b_11 = reader.ReadByte();
             bytearray_2 = reader.ReadBytes(5);
             statPair = reader.Read<StatPair>();
             u32_6 = reader.ReadUInt32();
-            u32_7 = reader.ReadUInt32();
+            GearLevel = reader.ReadUInt32();
             u32list_0 = reader.ReadList<UInt32>();
-            u16_4 = reader.ReadUInt16();
+            u16_2 = reader.ReadUInt16();
         }
     }
 }
