6c335ea2b76ce133f46f748a7b45a1902cb34316
9451589 <109340072+9451589@users.noreply.github.com>
2022-10-14 00:41:55 -0700

    PCStruct and StatusEffectData Fixes

--- LostArkLogger/Packets/Steam/PCStruct.cs
+++ LostArkLogger/Packets/Steam/PCStruct.cs
@@ -6,52 +6,49 @@ namespace LostArkLogger
     {
         public void SteamDecode(BitReader reader)
         {
+            str_0 = reader.ReadString(); //guild name
+            statusEffectDatas = reader.ReadList<StatusEffectData>();
             b_0 = reader.ReadByte();
-            u32_0 = reader.ReadUInt32();
             ClassId = reader.ReadUInt16();
-            EquippedItems = reader.ReadList<ItemInfo>();
-            b_12 = reader.ReadByte();
-            str_0 = reader.ReadString();
-            u16_3 = reader.ReadUInt16();
-            Name = reader.ReadString();
-            u32_7 = reader.ReadUInt32();
-            u32_8 = reader.ReadUInt32();
+            PlayerId = reader.ReadUInt64();
+            u32_0 = reader.ReadUInt32();
+            skillRunes = reader.Read<SkillRunes>();
+            subPKTNewNpc5 = reader.Read<subPKTNewNpc5>();
             b_1 = reader.ReadByte();
+            u32_1 = reader.ReadUInt32();
+            EquippedItems = reader.ReadList<ItemInfo>();
             b_2 = reader.ReadByte();
             b_3 = reader.ReadByte();
-            if (b_3 == 1)
-                bytearray_1 = reader.ReadBytes(12);
-            subPKTInitPC29s = reader.ReadList<subPKTInitPC29>();
-            u32_1 = reader.ReadUInt32();
-            u16_0 = reader.ReadUInt16();
             b_4 = reader.ReadByte();
             b_5 = reader.ReadByte();
-            PlayerId = reader.ReadUInt64();
             b_6 = reader.ReadByte();
+            u32_7 = reader.ReadUInt32();
             b_7 = reader.ReadByte();
-            u32_2 = reader.ReadUInt32();
             b_8 = reader.ReadByte();
-            u32_3 = reader.ReadUInt32();
             b_9 = reader.ReadByte();
-            PartyId = reader.ReadUInt64();
             b_10 = reader.ReadByte();
-            bytearray_0 = reader.ReadBytes(25);
-            itemInfos = reader.ReadList<ItemInfo>();
-            u64_0 = reader.ReadUInt64();
-            u32_4 = reader.ReadUInt32();
             Level = reader.ReadUInt16();
-            skillRunes = reader.Read<SkillRunes>();
-            u32_5 = reader.ReadUInt32();
-            subPKTNewNpc5 = reader.Read<subPKTNewNpc5>();
+            Name = reader.ReadString();
+            u16_0 = reader.ReadUInt16();
+            u32_2 = reader.ReadUInt32();
+            u32_3 = reader.ReadUInt32();
+            PartyId = reader.ReadUInt64();
             u16_1 = reader.ReadUInt16();
-            statusEffectDatas = reader.ReadList<StatusEffectData>();
-            b_11 = reader.ReadByte();
-            bytearray_2 = reader.ReadBytes(5);
-            statPair = reader.Read<StatPair>();
-            u32_6 = reader.ReadUInt32();
-            GearLevel = reader.ReadUInt32();
+            u32_4 = reader.ReadUInt32();
+            itemInfos = reader.ReadList<ItemInfo>();
+            bytearray_1 = reader.ReadBytes(5);
             u32list_0 = reader.ReadList<UInt32>();
             u16_2 = reader.ReadUInt16();
+            u16_3 = reader.ReadUInt16();
+            u32_5 = reader.ReadUInt32();
+            u64_0 = reader.ReadUInt64();
+            statPair = reader.Read<StatPair>();
+            bytearray_2 = reader.ReadBytes(5);
+            bytearray_0 = reader.ReadBytes(25);
+            u32_6 = reader.ReadUInt32();
+            subPKTInitPC29s = reader.ReadList<subPKTInitPC29>();
+            GearLevel = reader.ReadUInt32();
+            b_11 = reader.ReadByte();
         }
     }
 }
--- LostArkLogger/Packets/Steam/StatusEffectData.cs
+++ LostArkLogger/Packets/Steam/StatusEffectData.cs
@@ -6,20 +6,20 @@ namespace LostArkLogger
     {
         public void SteamDecode(BitReader reader)
         {
-            InstanceId = reader.ReadUInt64();
             SourceId = reader.ReadUInt64();
-            SkillLevel = reader.ReadByte();
+            InstanceId = reader.ReadUInt64();
+            b_1 = reader.ReadByte();
             hasValue = reader.ReadByte();
             if (hasValue == 1)
                 Value = reader.ReadBytes(16);
             b_0 = reader.ReadByte();
             if (b_0 == 1)
                 s64_1 = reader.ReadUInt64();
-            b_1 = reader.ReadByte();
+            SkillLevel = reader.ReadByte();
             u32_0 = reader.ReadUInt32();
             s64_0 = reader.ReadSimpleInt();
-            StatusEffectId = reader.ReadUInt32();
             EffectInstanceId = reader.ReadUInt32();
+            StatusEffectId = reader.ReadUInt32();
             bytearraylist_0 = reader.ReadList<Byte[]>(7);
         }
     }
