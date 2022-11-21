edb04c369af83c467f4c2a95e031b0ea42f2240d
guy0090 <4467272+guy0090@users.noreply.github.com>
2022-11-21 21:47:50 +0100

    Additional fix from https://github.com/shalzuth/LostArkLogger/commit/6d4d20c10b719d34cc8da36911c2bf2fd8954528#r90583953

--- LostArkLogger/Packets/Steam/PKTNewNpcSummon.cs
+++ LostArkLogger/Packets/Steam/PKTNewNpcSummon.cs
@@ -6,9 +6,9 @@ namespace LostArkLogger
     {
         public void SteamDecode(BitReader reader)
         {
-            b_0 = reader.ReadByte();
             bytearray_0 = reader.ReadBytes(18);
             OwnerId = reader.ReadUInt64();
+            b_0 = reader.ReadByte();
             bytearray_1 = reader.ReadBytes(13);
             npcStruct = reader.Read<NpcStruct>();
         }
